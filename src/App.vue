52c3ab894e3ca3e16e96e89238083a992dbe281d
Goncalves Raphael <raphael.goncalves87@hotmail.com>
2025-01-24 17:18:35 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="isLoading">Chargement...</div>
+  <div v-else class="container">
+    <Quiz :quiz="quiz" v-if="quiz"/>
+  </div>
+</template>
+
+<script setup>
+
+  // Récupérer les questions dans le fichier JSON
+  import { onMounted, ref } from 'vue';
+  import Quiz from './composant/Quiz.vue';
+
+  const quiz = ref(null);
+  const isLoading = ref(true);
+
+  onMounted(async () => {
+    try {
+      const response = await fetch('questionaire.json');
+      if (!response.ok) throw new Error(`Erreur ${response.status}: ${response.statusText}`);
+
+      const data = await response.json();
+      quiz.value = data;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des questions :', error);
+    } finally {
+      isLoading.value = false;
+    }
+  });
+
+
+
+</script>
+
+<style>
+  .container {
+    margin-top: 2rem;
+  }
+</style>
---
+++ src/composant/Quiz.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="quiz">
+        <h1>{{ quiz.title }}</h1>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    defineProps({
+        quiz: Object
+    })
+
+</script>
